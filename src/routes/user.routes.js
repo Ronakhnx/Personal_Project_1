dc30a40c4cebd1e5957238e4fbe148004003aacb
ronak_r <ronakradadiya4674@gmail.com>
2024-06-18 17:25:35 +0530

    "Removed duplicate exports in user.controller.js, updated import paths in auth.middleware.js, and added getWatchHistory route in user.routes.js"

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
   logOutUser,
   loginUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
+
 router.route("/register").post(
   upload.fields([
     { name: "avatar", maxCount: 1 },
@@ -16,8 +24,17 @@ router.route("/register").post(
   registerUser
 );
 router.route("/login").post(loginUser);
-router.route("/refresh-token").post(refreshAccessToken);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+
 export default router;
